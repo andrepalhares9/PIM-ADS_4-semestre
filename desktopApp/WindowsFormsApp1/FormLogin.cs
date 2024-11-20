87d418a43a7861d31190cbca35d541a8f8a72e90
SNeto99 <netoseno@hotmail.com>
2024-11-20 20:03:26 -0300

    up

--- desktopApp/WindowsFormsApp1/FormLogin.cs
+++ desktopApp/WindowsFormsApp1/FormLogin.cs
@@ -1,39 +1,64 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
+using System.Net.Http;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json;
 
 namespace WindowsFormsApp1
 {
     public partial class FormLogin : Form
     {
+        private readonly HttpClient client = new HttpClient();
+        private readonly string apiUrl = "https://pim.nseno.com/users/login"; // Ajuste a URL conforme sua API
+
         public FormLogin()
         {
             InitializeComponent();
+            txtSenha.PasswordChar = '*'; // Oculta a senha digitada
         }
 
-        private void btnAcesso_Click(object sender, EventArgs e)
+        private async void btnAcesso_Click(object sender, EventArgs e)
         {
-            String User = "Admin";
-            String Password = "Admin";
-            if (txtUsuario.Text == User & txtSenha.Text == Password)
+            try
             {
-                MessageBox.Show("Acesso liberado");
-                Form1 frnMain = new Form1();
-                frnMain.Show();
-                this.Hide();
+                var loginData = new
+                {
+                    userInput = txtUsuario.Text,
+                    passwordInput = txtSenha.Text
+                };
+
+                var content = new StringContent(
+                    JsonConvert.SerializeObject(loginData),
+                    Encoding.UTF8,
+                    "application/json"
+                );
+
+                var response = await client.PostAsync(apiUrl, content);
+                var responseString = await response.Content.ReadAsStringAsync();
+                var result = JsonConvert.DeserializeObject<LoginResponse>(responseString);
+
+                if (response.IsSuccessStatusCode && result.status == "ok")
+                {
+                    MessageBox.Show("Acesso liberado");
+                    Form1 frnMain = new Form1();
+                    frnMain.Show();
+                    this.Hide();
+                }
+                else
+                {
+                    MessageBox.Show("Usuário/Senha incorreta");
+                }
             }
-            else
-                MessageBox.Show("Usuário/Senha incoreta");
+            catch (Exception ex)
             {
-
+                MessageBox.Show($"Erro ao tentar fazer login: {ex.Message}");
             }
-
         }
+    }
+
+    public class LoginResponse
+    {
+        public string status { get; set; }
+        public string id { get; set; }
     }
 }
