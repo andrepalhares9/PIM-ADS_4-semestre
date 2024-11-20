984049e96be94074a4179de6e1656dde00ec465d
SNeto99 <netoseno@hotmail.com>
2024-11-20 19:22:33 -0300

    mt coisa, fds

---
+++ desktopApp/WindowsFormsApp1/ClassProdutos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    internal class ClassProdutos
+    {
+        public int id { get; set; }
+
+        public string produto { get; set; }
+        public int quantidade { get; set; } 
+
+     
+
+
+    }
+}
